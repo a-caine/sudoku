0e72bb8df3cc9bf6421e5870e692eac05e9f8017
Adam Caine <adam.caine10@gmail.com>
2022-11-24 16:34:02 +0000

    added input cleansing

--- main.c
+++ main.c
@@ -1,11 +1,46 @@
-#include <stdio.h>
 #include "generator.h"
+#include "main.h"
+
+#include <stdio.h>
+#include <ctype.h>
 
 int main() {
 
     printf("Hello world!\n");
 
-    printf("%d\n", getBoardSize());
+    struct board gameBoard = generateEmptyBoard();
+
+    printBoard(gameBoard);
+
+    char input[20];
+
+    scanf("%19s", input);
+
+    printf("%d",validateString(input));
+
+    return 0;
+}
+
+int validateString(char *s) {
+    // Check that all the character lie in the correct range
+
+    // Loop through each character
+
+    char c = *s;
+    int offset = 0;
+
+    while (c != '\0') {
+
+        char to_check = tolower(c);
+
+        printf("%c\n", c);
+        // Check that the characters lie in the correct range
+        if (!((to_check > 47 && to_check < 58) || (to_check > 96 && to_check < 123))) return -1;
+
+        c = *(s + ++offset);
+
+    }
+
 
     return 0;
 }
---
+++ main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+int main();
+
+int validateString(char *s);
+
+
+#endif
