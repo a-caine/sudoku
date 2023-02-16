0e1dd381d9cfff6f8d0a15002332e3ae1039f100
Adam Caine <adam.caine10@gmail.com>
2023-02-16 14:12:45 +0000

    Added methods for checking if a stack contains a number or not

--- helpers.c
+++ helpers.c
@@ -22,6 +22,15 @@ int charToNum(char c) {
 
 }
 
+uint8_t init(struct num_stack *stack) {
+    // Initialise the stack with data
+    stack->stack = calloc(sizeof(uint8_t), 256);
+}
+
+uint8_t delete(struct num_stack * stack) {
+    free(stack->stack);
+}
+
 uint8_t pop(struct num_stack *stack) {
     stack->head -= 1;
     if (stack->head == 255) {
@@ -40,4 +49,32 @@ void push(struct num_stack *stack, uint8_t data) {
     }
 
     *(stack->stack + stack->head) = data;
+}
+
+
+_Bool contains(struct num_stack *stack, uint8_t data) {
+    for (uint8_t i = 0; i < stack->head; i++) {
+        if (*(stack->stack + i) == data) return 1;
+    }
+
+    return 0;
+}
+
+uint8_t randSudNum(struct num_stack *stack) {
+    // First generate a random number
+
+    uint8_t randNum = 0;
+
+    // If the number is 0 or is in the stack already, keep trying to generate the number
+    while (!randNum || contains(stack, randNum)) {
+
+        randNum = rand() % 10;
+
+    }
+
+    // Return the number and add it to the stack
+    // TODO: remove this
+    push(stack, randNum);
+
+    return randNum;
 }
--- helpers.h
+++ helpers.h
@@ -2,6 +2,7 @@
 #define HELPERS_H
 
 #include <stdint.h>
+#include <stdlib.h>
 
 struct num_stack {
     uint8_t *stack;
@@ -11,8 +12,19 @@ struct num_stack {
 
 int charToNum(char c);
 
+uint8_t init(struct num_stack *stack);
+
+uint8_t delete(struct num_stack *stack);
+
+// Removes the top number from the number stack
 uint8_t pop(struct num_stack *stack);
 
+// Adds a number to the top of the number stack
 void push(struct num_stack *stack, uint8_t data);
+
+// Checks whether the num_stack contains a given number
+_Bool contains(struct num_stack *stack, uint8_t data);
+
+uint8_t randSudNum(struct num_stack *stack);
 
 #endif
